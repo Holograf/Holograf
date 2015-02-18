626c1c0be19e708fce12450822260ebbbf985b61
Luke Davis <lukefable@gmail.com>
2015-02-18 19:05:06 +0000

    Resolves conflicts with gulpfile and package json

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var concat = require('gulp-concat');
 var shell = require('gulp-shell');
-var sass = require('gulp-sass') ;
 var rename = require('gulp-rename');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
@@ -14,22 +13,10 @@ var paths = {
   scripts: ['public/**/*.js'],
   html: ['public/**/*.html'],
   server: ['server/**/*.js'],
-  test: ['specs/**/*.js'],
-  sass: ['public/scss/style.scss']
+  test: ['specs/**/*.js']
 };
 
 
-gulp.task('sass', function(done) {
-  return gulp.src(paths.sass)
-    .pipe(sass())
-    .pipe(gulp.dest('./dist/css/'))
-    .pipe(sass({sourcemap: true}))
-    // .pipe(minifyCss({
-    //   keepSpecialComments: 0
-    // }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./dist/css/'));
-});
 gulp.task('copy', function () {
   return gulp.src(paths.html)
     .pipe(gulp.dest('dist/'));
