210f4e14b058cf6b60725e48275c6d93fda33029
mc <mc@mcs-macbook-pro.gateway.2wire.net>
2015-02-17 23:38:32 -0800

    Adds button interactions (including ones manipulating 3js scene) and css/bootstrap styling
    Moves unused components to folder
    
    Adds functionality & interactivity to buttons; still unable to properly change tabs to 3D visualization automatically
    
    Adds shareUrl to getAppState
    
    Adds interaction to buttons; cleans up console output
    
    Updates argument passed to the displayScene Three.js function

--- public/js/components/CodeEditor.js
+++ public/js/components/CodeEditor.js
@@ -3,8 +3,8 @@
  */
 
 var React = require('react');
+var addons = require('react-addons');
 var Panel = require('react-bootstrap/Panel');
-// var Router = require('react-router');
 var CodeMirror = require('./CodeMirror/');
 var Button = require('react-bootstrap/Button');
 var Input = require('react-bootstrap/Input');
@@ -29,38 +29,46 @@ module.exports = React.createClass({
   },
 
   compile: function () {
-    // Actions.compile(function(data) {  // });
-    deleteScene();
+    {this.props.tabKey}
+    // console.log('this.props:',this.props);
     Actions.compile();
   },
-
-  // getInitialState: function () {
-  //   console.log('initializing')
-  //   if (this.getParams().id) {
-  //     this.fetch(this.getParams().id);
-  //   }
-  // },
-
-  // componentWillReceiveProps: function () {
-  //   console.log('receiving props');
-  //   // this.fetch(this.getParams().id); // TODO: Fix repeating call on Actions.updateCode
-  // },
 
   save: function () {
     var data = {rawCode: this.props.code, processedCode: 'processedCode'};
     Actions.updateUrl(data);
   },
 
+  refresh: function() {
+    history.go(0);
+  },
+
+// bsStyle options: ["default","primary","success","info","warning","danger","link","inline","tabs","pills"]. 
   render: function () {
+    // dynamic classes for the buttons
+    var compileClasses = addons.classSet({
+      'pull-right': true,
+      'codeButton': true,
+      'disabled': this.props.compiledStatus
+    });
+    var resetClasses = addons.classSet({
+      'pull-right': true,
+      'codeButton': true,
+      'hidden': !this.props.compiledStatus
+    });
+    var shareClasses = addons.classSet({
+      'disabled': !this.props.compiledStatus
+    });
+
 
     this.options.value = this.props.code;
 
-    // <Panel className="codeMirrorPanel">        </Panel>
     return (
       <div className="codeContainer">
-          <CodeMirror {...this.options} />
-        <Col xs={6} md={4}><Input readOnly type="text" value={this.props.shareUrl} buttonBefore={<Button onClick={this.save} >Share</Button>} /></Col>
-        <Button bsStyle="primary" className={'pull-right'} onClick={this.compile} >Compile</Button>
+        <CodeMirror {...this.options} />
+        <Col xs={6} md={4}><Input readOnly type="text" value={this.props.shareUrl} buttonBefore={<Button onClick={this.save} className={shareClasses}>Share</Button>} /></Col>
+        <Button bsStyle="primary" onClick={this.compile} className={compileClasses} >Compile</Button>
+        <Button bsStyle="danger" onClick={this.refresh} className={resetClasses} >Reset Code</Button>
       </div>
     );
   }
