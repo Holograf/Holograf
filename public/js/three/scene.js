f9707954293b215e5c62bf2578ff28ccfc0b18c2
mc <mc@mcs-MacBook-Pro.local>
2015-02-20 19:50:11 -0800

    Tests controls and consolidates Three.js files some more
    Conflicts:
    	public/js/three/scene.js
    
    Conflicts:
    	public/js/three/scene.js
    
    update renderer.js
    
    Conflicts:
    	public/js/three/renderer.js
    
    Tests Three controls; edits codeEditor styles
    
    Conflicts:
    	public/js/three/scene.js
    
    Conflicts:
    	dist/three/threeMaster.js
    	dist/threeLib/three-r70.js
    
    Controls updates & gitignore updates
    
    Removes merge conflict from scene.js
    
    Small updates to custom controls
    
    Conflicts:
    	dist/three/customControls.js
    	public/index.html
    
    Adds some of controls to scene.js; works on gulpfile trying to browserify three files
    
    Conflicts:
    	gulpfile.js
    	public/js/three/controls/controls-old.js
    	public/js/three/scene.js
    
    Edits controls and scene further; attempts gulptask to browserify three files, but still working on it
    
    Conflicts:
    	gulpfile.js
    
    Adds more controls of the controls
    
    Conflicts:
    	public/js/three/scene.js
    
    Adds files for beanstalk deployment
    
    Tests more controls possibilities; trying to get camera rotation with keys down
    
    Conflicts:
    	public/js/three/scene.js
    
    Works on control experiments
    
    Conflicts:
    	public/js/three/scene.js
    
    Resizes canvas height to fit inside window
    
    Adds basic key controls
    
    Conflicts:
    	public/js/three/scene.js
    
    Adds camera controls; only not working with up and down arrow keys
    
    Conflicts:
    	public/js/three/scene.js
    
    Resolves merge conflicts
    
    Adds control rotation with target, lookAt
    
    Gets some good looking controls working
    
    Improves controls; adds zoom in to element when clicked
    
    Gets tweening to move camera to clicked object; Updates controls
    
    Adds cross-hatched walls on back and bottom of scene
    
    Causes camera to former position from nodeView

--- public/js/three/scene.js
+++ public/js/three/scene.js
@@ -1,55 +1,77 @@
-var theatre={scenePaused:false,expanded:false};
+var theatre = {
+	scenePaused: false, 
+	expanded: false, 
+	controlsEnabled: true, 
+	nodeView: false
+};
 
-theatre.display=function(allData){	
+theatre.display = function(allData){	
 	var composite, container, controls, camera, scene, renderer, particleLight, tween, visualTimeline;
 	var windowHalfX = window.innerWidth / 2;
 	var windowHalfY = window.innerHeight / 2;
-	var centerPoint;
-	var scopes=utils.extractScopes(allData);
-	var timeline=utils.parseTimeline(allData.programSteps,allData.components);
+	var scopes = utils.extractScopes(allData);
+	var timeline = utils.parseTimeline(allData.programSteps, allData.components);
 	
 	init(timeline);
 	animate();
 	
 	function init(data) {
-		container = document.getElementById('three-scene');
 
+		// PerspectiveCamera method args: (field of view angle, aspectRatio, near, far)
+		camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100000 );
+		camera.position.x = -3000;
+		camera.position.y = 3000;
+		camera.position.z = 5000;
+
+		controls = new THREE.OrbitControls(camera, container);
+		// controls.addEventListener( 'change', render );
+		
 		scene = new THREE.Scene();
 		
-		composite = subroutines.Composite(data,scopes);
-		centerPoint = new THREE.Vector3(0,0,composite.maxSize/2);
-		scene.add( composite );
-		
-		particleLight = subroutines.TimeLight(0,composite.maxSize);
+		// theatre.camera = camera;
+		// theatre.target = target;
+
+
+		// timeline elements
+
+		particleLight = subroutines.TimeLight();
 		particleLight.tween.start();
 		particleLight.tween.onComplete(function(){
 			particleLight.position.z=0;
 			particleLight.tween.start();
 		});
 		scene.add( particleLight );
-		
-		camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, Math.max(100000,composite.maxSize) );
-		camera.position.z = composite.maxSize/2;
-		camera.position.y = 0;
-		camera.position.x = Math.min(-2000,-1*(composite.maxSize/3) );
-		
-		
-		controls = new THREE.OrbitControls(camera, container);
-		controls.addEventListener( 'change', render );
-		
+
+		composite = subroutines.Composite(data,scopes);
+		scene.add( composite );
+
 		visualTimeline = subroutines.VisualTimeline(data,scopes);
 		scene.add(visualTimeline);
+
 		//will add the dotgrid to the scene;
 		subroutines.dotGrid(scene,data,scopes,composite.maxSize);
+		subroutines.skybox(scene, composite.maxSize);
+
+		// scene.add(subroutines.Axes()[0]);
+		// scene.add(subroutines.Axes()[1]);
+		// scene.add(subroutines.Axes()[2]);
+
+
+		// renderer
 		
-		renderer = new THREE.WebGLRenderer();
-			renderer.setPixelRatio( window.devicePixelRatio );
-			renderer.setSize( window.innerWidth, window.innerHeight-$(container).offset().top );
-			renderer.setClearColor( 0x333333, 1);
-			container.appendChild( renderer.domElement );
+		renderer = new THREE.WebGLRenderer({antialias:true});
+		renderer.setClearColor( 0x333333, 1);
+		renderer.setPixelRatio( window.devicePixelRatio );
+		renderer.setSize( window.innerWidth, window.innerHeight - 86);  // hard-coded top offset
+		// renderer.setSize( window.innerWidth, window.innerHeight-$(container).offset().top );
+	
+		container = document.getElementById('three-scene');
+		container.appendChild(renderer.domElement);
+
 		// User interaction
 		window.addEventListener( 'mousemove', onMouseMove, false );
 		window.addEventListener( 'resize', onWindowResize, false );
+		window.addEventListener( 'mousedown', onMouseDown, false);
 	}
 
 	function onWindowResize() {
@@ -58,6 +80,7 @@ theatre.display=function(allData){
 		camera.aspect = window.innerWidth / window.innerHeight;
 		camera.updateProjectionMatrix();
 		renderer.setSize( window.innerWidth, window.innerHeight );
+		// render();
 	}
 
 	function onMouseMove( e ) {
@@ -107,18 +130,79 @@ theatre.display=function(allData){
 		
 	}
 
-	
+	function onMouseDown () {
+		var cameraSpeed = 1500;
+
+		if (theatre.nodeView) {
+
+			new TWEEN.Tween(camera.position).to(theatre.lastPosition, cameraSpeed).start();
+			new TWEEN.Tween( camera.rotation ).to(theatre.lastRotation, cameraSpeed).start();
+			theatre.nodeView = false;
+		}
+
+		var vector = new THREE.Vector3();
+		var raycaster = new THREE.Raycaster();
+		var dir = new THREE.Vector3();
+
+		//check the type of camera
+		//extract that offset into an external variable that doesn't have to be recalculated every time... later
+		var x =  ( event.clientX / window.innerWidth ) * 2 - 1;
+		var y = - ( (event.clientY-$(container).offset().top ) / window.innerHeight ) * 2 + 1;
+		if ( camera instanceof THREE.OrthographicCamera ) {
+	    vector.set( x, y, - 1 ); // z = - 1 important!
+	    vector.unproject( camera );
+	    dir.set( 0, 0, - 1 ).transformDirection( camera.matrixWorld );
+	    raycaster.set( vector, dir );
+		} else if ( camera instanceof THREE.PerspectiveCamera ) {
+	    vector.set( x, y, 0.5 ); // z = 0.5 important!
+	    vector.unproject( camera );
+	    raycaster.set( camera.position, vector.sub( camera.position ).normalize() );
+		}
+		
+		if (composite && !theatre.nodeView){
+			var intersects = raycaster.intersectObjects( composite.children, true );	
+			if (intersects.length > 0) { 
+
+				// save initial camera position for return
+				theatre.lastPosition = new THREE.Vector3().copy( camera.position );
+				theatre.lastRotation = new THREE.Quaternion().copy( camera.rotation );
+				// final camera position
+				var newX = intersects[0].point.x - 600;
+				var newY = intersects[0].point.y + 600;
+				var newZ = intersects[0].point.z - 300;
+				var targetPosition = new THREE.Vector3(newX, newY, newZ);
+
+
+				// camera rotation
+					// use extra camera to find rotation at target location
+				var nextCamera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 100000);
+				nextCamera.position.x = targetPosition.x;
+				nextCamera.position.y = targetPosition.y;
+				nextCamera.position.z = targetPosition.z;
+				nextCamera.lookAt(intersects[0].point);
+				var endRotation = new THREE.Quaternion().copy( nextCamera.rotation );
+
+				// camera motion on click - position & rotation
+				new TWEEN.Tween(camera.position).to(targetPosition, cameraSpeed).start();
+				new TWEEN.Tween( camera.rotation ).to(endRotation, cameraSpeed).start();
+				nextCamera = null;
+				theatre.nodeView = true;
+			}
+		}
+	}
+
+
 	function animate() {
 		requestAnimationFrame( animate );
-		controls.update();
+		// controls.update();
 		render();
 	}
-	
+
 	theatre.pause=function(){
 		theatre.scenePaused ? particleLight.tween.start() : particleLight.tween.stop();
 		theatre.scenePaused=!theatre.scenePaused;
 	};
-	
+
 	theatre.expand=function(){
 		var action = theatre.expanded ? "collapse" : "expand";
 		for (var i=0;i<composite.children.length;i++){
@@ -131,12 +215,11 @@ theatre.display=function(allData){
 		}
 		theatre.expanded=!theatre.expanded;
 	};
-	
+
+
 	function render() {
-		camera.lookAt(centerPoint);
 		TWEEN.update();
 		renderer.render( scene, camera );
-		sceneRendered = true;
+		// effect.render( scene, camera );			// This is used for stereoEffect
 	}
-	
 };
