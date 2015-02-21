59a1287a1087f91bd4a810399221190ff4783200
mc <mc@mcs-MacBook-Pro.local>
2015-02-20 20:01:00 -0800

    update renderer.js

--- public/js/three/renderer.js
+++ public/js/three/renderer.js
@@ -5,10 +5,10 @@
 var makeRenderer = function() {
 
   // current code:
-  // var renderer = new THREE.CanvasRenderer();
+  var renderer = new THREE.CanvasRenderer();
 
   // http://codepen.io/nireno/pen/cAoGI?editors=001
-  renderer = new THREE.WebGLRenderer( { antialias: false } );
+  // renderer = new THREE.WebGLRenderer( { antialias: false } );
 
   renderer.setClearColor( 0x333333, 1);
   renderer.setSize( window.innerWidth - 20, window.innerHeight - 20 );
