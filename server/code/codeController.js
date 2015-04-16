73570f31ad9dd25823e7ea84d5e65fb81372ce79
Andy Coenen <andrew.coenen@gmail.com>
2015-04-16 09:40:22 -0700

    Temporarily disables mongoDB connectivity

--- server/code/codeController.js
+++ server/code/codeController.js
@@ -1,4 +1,4 @@
-var Code = require('./codeModel.js');
+// var Code = require('./codeModel.js');
 
 module.exports = {
   findId: function(req, res, next, id){
@@ -8,22 +8,24 @@ module.exports = {
 
   addCode: function(req, res, next){
     res.header('Access-Control-Allow-Origin', "*");
-    new Code(req.body).save(function(err, code, numAffected){
-      if (err) {
-        return res.sendStatus(500);
-      }
-      return res.status(200).send({'code': code._id});
-    });
+    // new Code(req.body).save(function(err, code, numAffected){
+    //   if (err) {
+    //     return res.sendStatus(500);
+    //   }
+    //   return res.status(200).send({'code': code._id});
+    // });
+    return res.status(200)
   },
 
   getCode: function(req, res, next){
     res.header('Access-Control-Allow-Origin', "*");
-    var id = req.id;
-    Code.find({_id: id}, function(err, results){
-      if (err) {
-        res.sendStatus(500);
-      }
-      res.send(results);
-    });
+    // var id = req.id;
+    // Code.find({_id: id}, function(err, results){
+    //   if (err) {
+    //     res.sendStatus(500);
+    //   }
+    //   res.send(results);
+    // });
+    return res.status(200)
   }
 };
